f23696160be572be84d5d70a26e75e9bbd8bdd27
Oludemilade <Rajiol>
2016-06-27 13:05:14 -0700

    Changed user Interface to only one view
    Added error checks.

--- What's The weather/ViewController.swift
+++ What's The weather/ViewController.swift
@@ -14,8 +14,11 @@ extension NSString {
         return components.filter { !$0.isEmpty }.joinWithSeparator(" ")
     }
 }
+
+
+
 var finalURL = NSURL(string: "")
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     
     
@@ -23,6 +26,7 @@ class ViewController: UIViewController {
     
     var valueCity:String = ""
     
+    @IBOutlet var weatherSummary: UILabel!
     @IBAction func checkWeather(sender: AnyObject) {
         let url1 = "http://www.weather-forecast.com/locations/"
         let url2 = "/forecasts/latest"
@@ -42,18 +46,28 @@ class ViewController: UIViewController {
         let url3 = url1 + valueCity + url2
         finalURL = NSURL(string: url3)!
         
-        let task = NSURLSession.sharedSession().dataTaskWithURL(finalURL!) { (data, response, error) -> Void in
+        if (finalURL != nil) {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(finalURL!) { (data, response, error) -> Void in
             //Will happen when task completes.
             if let UrlContent = data {
                 let webContent = NSString(data: UrlContent, encoding: NSUTF8StringEncoding)
+                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
                 
+                if websiteArray.count >  1 {
+                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 1 {
+                        let weatherSum = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString:"º" )
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.weatherSummary.text = weatherSum
+                        })
+                    }
+                }
+                else
+                {
+                    self.weatherSummary.text = "Please Enter a Valid City"
+                }
                 
-                print(webContent)
-                
-                /* //Sometimes Queue takes a long time. We can force the queue to display
-                 dispatch_async(dispatch_get_main_queue(), {
-                 self.webView.loadHTMLString(String(webContent!), baseURL: nil)
-                 })*/
                 
             }
             else
@@ -62,12 +76,13 @@ class ViewController: UIViewController {
             }
         }
         task.resume()
+        }
 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.userInputCity.delegate = self
 
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -76,6 +91,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
      
+    }
+    
+    //if user presses outside of the keyboard, it hides
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        self.view.endEditing(true)
+    }
+    
+    //if user presses the return it should disappear
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
     }
 
 }
